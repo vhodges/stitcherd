768b06d40477e2b133672112b9debdb5c79d59e5
Vince Hodges <vhodges@gmail.com>
2020-11-26 09:38:36 -0800

    Remove unused interface

--- stitcher/renderable_content.go
+++
@@ -1,6 +0,0 @@
-package stitcher
-
-// RenderableContent is anything that returns a string to be merged into a document
-type RenderableContent interface {
-	Render(fetchcontext map[string]interface{}) (string, error)
-}
